d5d9777571f2119b1a3ccaf5f365b1d33cd2cf51
Josef Spillner <spio@tiguriner>
2016-05-19 08:24:41 +0200

    - removal of experiments scripts; not needed for the further implementation

--- experiments/calc-availability.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd ..
-./calc-availability.py availabilities > plotting/calc-availability.csv.availabilities
-./calc-availability.py variances > plotting/calc-availability.csv.variances
